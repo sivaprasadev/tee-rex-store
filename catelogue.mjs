4eaf7b0b0b03608e74a72dec944542cb440d38cb
SIVAPRASAD E V <sivaprasad.ev@capfront.in>
2022-10-12 14:49:06 +0530

    filter for price also added

---
+++ catelogue.mjs
@@ -0,0 +1,83 @@
+export const catelogue = [
+	{
+		id: 1,
+		name: 'Black Polo',
+		type: 'Polo',
+		price: 500,
+		currency: 'INR',
+		color: 'Black',
+		gender: 'Men',
+		quantity: 3
+	},
+	{
+		id: 2,
+		name: 'Blue Polo',
+		type: 'Polo',
+		price: 130,
+		currency: 'INR',
+		color: 'Blue',
+		gender: 'Men',
+		quantity: 3
+	},
+	{
+		id: 3,
+		name: 'Pink Polo',
+		type: 'Polo',
+		price: 10,
+		currency: 'INR',
+		color: 'Pink',
+		gender: 'Women',
+		quantity: 6
+	},
+	{
+		id: 4,
+		name: 'Black Hoodie',
+		type: 'Hoodie',
+		price: 200,
+		currency: 'INR',
+		color: 'Black',
+		gender: 'Women',
+		quantity: 2
+	},
+	{
+		id: 4,
+		name: 'Black Hoodie',
+		type: 'Hoodie',
+		price: 1000,
+		currency: 'INR',
+		color: 'Black',
+		gender: 'Men',
+		quantity: 2
+	},
+	{
+		id: 7,
+		name: 'Blue Hoodie',
+		type: 'Hoodie',
+		price: 500,
+		currency: 'INR',
+		color: 'Blue',
+		gender: 'Men',
+		quantity: 2
+	},
+
+	{
+		id: 9,
+		name: 'Blue Round',
+		type: 'Basic',
+		price: 320,
+		currency: 'INR',
+		color: 'Blue',
+		gender: 'Men',
+		quantity: 2
+	},
+	{
+		id: 10,
+		name: 'Red Round',
+		type: 'Basic',
+		price: 250,
+		currency: 'INR',
+		color: 'Red',
+		gender: 'Women',
+		quantity: 2
+	}
+];
---
+++ test.mjs
@@ -0,0 +1,36 @@
+import { catelogue } from './catelogue.mjs';
+
+const filterData = {
+	type: ['Polo', 'Hoodie', 'Basic'],
+	color: ['Red'],
+	gender: ['Men', 'Women'],
+	price: [250]
+};
+
+function filterArray(targetArray, filters) {
+	let filterKeys = Object.keys(filters);
+	let result;
+
+	function filterCatelogue(cat) {
+		return filterKeys.every((keys) => {
+			if (!filters[keys].length) return true;
+
+			if (keys === 'price') {
+				if (filters.price.indexOf(250) !== -1) {
+					return cat.price > 0 && cat.price <= 250;
+				} else if (filters.price.indexOf(251) !== -1) {
+					return cat.price > 250 && cat.price <= 500;
+				} else {
+					return cat.price > 500 && cat.price <= 1000;
+				}
+			}
+
+			return filters[keys].includes(cat[keys]);
+		});
+	}
+
+	result = targetArray.filter(filterCatelogue);
+	return result;
+}
+
+console.log('filterArray:', filterArray(catelogue, filterData));
